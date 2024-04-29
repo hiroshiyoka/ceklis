678aee24f74d0c48f37509d2434e7617dbceddbd
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-04-30 00:13:24 +0700

    feat(home): set background color using tdBGColor constant

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -7,6 +7,7 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: tdBGColor,
       appBar: AppBar(
         backgroundColor: tdBGColor,
         elevation: 0,
