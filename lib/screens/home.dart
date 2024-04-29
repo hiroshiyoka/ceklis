3553798a64401ed3134c4157a421251b27ecd644
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-04-30 00:15:24 +0700

    feat(home): refactor appBar function to extract method

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -8,29 +8,32 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: tdBGColor,
-      appBar: AppBar(
-        backgroundColor: tdBGColor,
-        elevation: 0,
-        title:
-            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-          Icon(
-            Icons.menu,
-            color: tdBlack,
-            size: 30,
-          ),
-          Container(
-            height: 40,
-            width: 40,
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(20),
-              child: Image.asset('asset/images/about.jpg'),
-            ),
-          )
-        ]),
-      ),
+      appBar: _buildAppBar(),
       body: Container(
         child: Text('This is home screen.'),
       ),
+    );
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      backgroundColor: tdBGColor,
+      elevation: 0,
+      title: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+        Icon(
+          Icons.menu,
+          color: tdBlack,
+          size: 30,
+        ),
+        Container(
+          height: 40,
+          width: 40,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: Image.asset('asset/images/about.jpg'),
+          ),
+        )
+      ]),
     );
   }
 }
