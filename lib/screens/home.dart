ed6d92ff6688b0e8b475de54421ad4a48b4b7818
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-05-09 08:03:45 +0700

    feat(home): add boxShadow to container in Home screen

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -51,6 +51,18 @@ class Home extends StatelessWidget {
                     right: 20,
                     left: 20,
                   ),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Colors.grey,
+                        offset: Offset(0.0, 0.0),
+                        blurRadius: 10.0,
+                        spreadRadius: 0.0,
+                      ),
+                    ],
+                    borderRadius: BorderRadius.circular(10),
+                  ),
                 ))
               ],
             ),
