04080f2fa8ce668c8100bcee5835000822900757
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-05-09 08:06:36 +0700

    feat(home): add text field for adding new activities

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -45,25 +45,32 @@ class Home extends StatelessWidget {
             child: Row(
               children: [
                 Expanded(
-                    child: Container(
-                  margin: EdgeInsets.only(
-                    bottom: 20,
-                    right: 20,
-                    left: 20,
-                  ),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    boxShadow: const [
-                      BoxShadow(
-                        color: Colors.grey,
-                        offset: Offset(0.0, 0.0),
-                        blurRadius: 10.0,
-                        spreadRadius: 0.0,
+                  child: Container(
+                    margin: EdgeInsets.only(
+                      bottom: 20,
+                      right: 20,
+                      left: 20,
+                    ),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Colors.grey,
+                          offset: Offset(0.0, 0.0),
+                          blurRadius: 10.0,
+                          spreadRadius: 0.0,
+                        ),
+                      ],
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Tambahkan kegiatan baru Anda',
+                        border: InputBorder.none,
                       ),
-                    ],
-                    borderRadius: BorderRadius.circular(10),
+                    ),
                   ),
-                ))
+                ),
               ],
             ),
           )
