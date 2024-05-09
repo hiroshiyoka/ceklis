1c2b9ee74c278f044cd6c411349c74bfa0e0aac2
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-05-09 07:56:12 +0700

    feat(home): wrapped body container to Stack column

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -14,28 +14,33 @@ class Home extends StatelessWidget {
     return Scaffold(
       backgroundColor: tdBGColor,
       appBar: _buildAppBar(),
-      body: Container(
-        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-        child: Column(children: [
-          searchBox(),
-          Expanded(
-            child: ListView(
-              children: [
-                Container(
-                  margin: EdgeInsets.only(top: 50, bottom: 20),
-                  child: Text(
-                    'Semua kegiatanmu',
-                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.w500),
-                  ),
+      body: Stack(
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+            child: Column(children: [
+              searchBox(),
+              Expanded(
+                child: ListView(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(top: 50, bottom: 20),
+                      child: Text(
+                        'Semua kegiatanmu',
+                        style: TextStyle(
+                            fontSize: 30, fontWeight: FontWeight.w500),
+                      ),
+                    ),
+                    for (ToDo todo in todoList)
+                      ToDoItem(
+                        todo: todo,
+                      ),
+                  ],
                 ),
-                for (ToDo todo in todoList)
-                  ToDoItem(
-                    todo: todo,
-                  ),
-              ],
-            ),
-          )
-        ]),
+              )
+            ]),
+          ),
+        ],
       ),
     );
   }
